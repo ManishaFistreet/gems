e788a233a215baf4087219136e912963ab43136a
ManishaFistreet <manisha@fistreet.in>
2025-07-31 18:19:39 +0530

    changes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -12,7 +12,7 @@ function Register() {
 
     const handleRegister = async () => {
         try {
-            const response = await fetch(`${baseUrl}/api/auth/register`, {
+            const response = await fetch(`${baseUrl}/auth/register`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formData),
